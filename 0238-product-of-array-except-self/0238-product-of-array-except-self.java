7aeb1e3fd4ebd2cf864e49f1db34e67a4f7d8457
maeng24 <57695863+maeng24@users.noreply.github.com>
2024-08-31 15:54:58 +0900

    Time: 2 ms (86.54%), Space: 55.1 MB (80.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 class Solution {
@@ -6,14 +6,14 @@ class Solution {
         int multNums = 1;
         int zeroNum = 0;
         int len = nums.length;
-        ArrayList<Integer> zeroIndex = new ArrayList();
+        int zeroIndex = 0;
 
         for(int i=0;i<len;i++){
             if(nums[i]!=0){
                 multNums*=nums[i];
             }else{
                 zeroNum++;
-                zeroIndex.add(i);
+                zeroIndex=i;
             }
         }
 
@@ -23,7 +23,7 @@ class Solution {
             Arrays.fill(answer, 0);
         }else if(zeroNum == 1){
             Arrays.fill(answer, 0);
-            answer[zeroIndex.get(0)] = multNums;
+            answer[zeroIndex] = multNums;
         }else{
             for(int i=0; i<len; i++){
                 answer[i]=multNums/nums[i];
